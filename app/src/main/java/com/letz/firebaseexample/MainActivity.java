32cde094f1da78ea6edf2d353ddf8d475dd65375
letz <letz0703@gmail.com>
2021-09-05 20:17:53 +0900

    firebase login
    done

--- app/src/main/java/com/letz/firebaseexample/MainActivity.java
+++ app/src/main/java/com/letz/firebaseexample/MainActivity.java
@@ -9,7 +9,12 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -22,6 +27,8 @@ public class MainActivity extends AppCompatActivity
     EditText password;
     Button SignIn;
     Button SignUp;
+
+    FirebaseAuth auth = FirebaseAuth.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,7 +44,9 @@ public class MainActivity extends AppCompatActivity
         {
             @Override
             public void onClick(View v) {
-
+                String userMail = mail.getText().toString();
+                String userPassword = password.getText().toString();
+                signInFirebase(userMail, userPassword);
             }
         });
 
@@ -49,8 +58,29 @@ public class MainActivity extends AppCompatActivity
                 startActivity(iSignUp);
             }
         });
+    }
+
+    public void signInFirebase(String userMail, String userPassword) {
+        auth.signInWithEmailAndPassword(userMail, userPassword)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>()
+                {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Intent i = new Intent(MainActivity.this, main_menu.class);
+                            startActivity(i);
+                            finish();
 
 
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(MainActivity.this
+                                    , "mail and password is not correct"
+                                    , Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
 
     }
 }
