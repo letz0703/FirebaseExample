f0f81882b9d8838cebdafcd5cea33deb5dbb28c5
letz0703 <letz0703@gmail.com>
2021-09-06 10:57:29 +0900

    signIn 유지

--- app/src/main/java/com/letz/firebaseexample/MainActivity.java
+++ app/src/main/java/com/letz/firebaseexample/MainActivity.java
@@ -15,6 +15,7 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -82,5 +83,18 @@ public class MainActivity extends AppCompatActivity
                     }
                 });
 
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseUser user = auth.getCurrentUser();
+        if (user != null)
+        {
+            Intent i = new Intent(MainActivity.this, main_menu.class);
+            startActivity(i);
+            finish();
+        }
     }
 }
