6e92b553a6d355c3f1f1ac9fdac6f58c5948e19c
letz0703 <letz0703@gmail.com>
2021-09-07 10:06:26 +0900

    implement FCM
    register FCM
    
    FCM customize
    
    <meta-data> chang notificaion icon
    font coor -> color.xml
    
    Update AndroidManifest.xml

---
+++ app/src/main/java/com/letz/firebaseexample/MyFirebaseService.java
@@ -0,0 +1,15 @@
+package com.letz.firebaseexample;
+
+import androidx.annotation.NonNull;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class MyFirebaseService extends FirebaseMessagingService
+{
+    @Override
+    public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+
+    }
+}
